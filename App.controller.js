cd48f68ce823e08e6ad4feb303eaff727dbb3016
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2019-11-11 17:18:04 +0500

    Add files via upload

--- App.controller.js
+++ App.controller.js
@@ -10,7 +10,31 @@ sap.ui.define([
 			MessageBox.alert("Link was clicked!");
 		},
 		onInit: function (){
-			MessageBox.alert("This done got initialized ma ni*&^r!");
+			//MessageBox.alert("This done got initialized ma ni*&^r!");
+			var oModel, oView;
+			     var jsonData = [ {
+							 a: 1,
+						b:"ammar"
+						 }, {
+							 a: 2,
+						b:"ali"
+						 }, {
+							 a: 3,
+						b:"omer"
+						 }, {
+							 a: 4,
+						b:"saad"
+						 }, {
+							 a:  5,
+						b:"waqar"
+						 }, {
+							 a: 6,
+						b:"babar"
+				 }];
+			var jsonModel = new sap.ui.model.json.JSONModel(jsonData);
+			oView = this.getView();
+			oView.setModel(jsonModel);
+			
 		}
 		
 
