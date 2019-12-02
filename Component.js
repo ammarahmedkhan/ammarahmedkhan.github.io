bbc87a351c727a2fb01719031017b9b0744d6d67
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2019-12-02 12:09:37 +0500

    Add files via upload

--- Component.js
+++ Component.js
@@ -1,9 +1,10 @@
-sap.ui.define(['sap/ui/core/UIComponent'],
-	function(UIComponent) {
+sap.ui.define(['sap/ui/core/UIComponent', 'sap/ui/core/BusyIndicator' ],
+	function(UIComponent,BusyIndicator) {
 	"use strict";
-
+	console.log("Component:" + Date.now());
+	BusyIndicator.show();
 	var Component = UIComponent.extend("sap.m.sample.sampleapp4.controller.Component", {
-
+		
 		metadata : {
 			manifest: "json"
 		}
