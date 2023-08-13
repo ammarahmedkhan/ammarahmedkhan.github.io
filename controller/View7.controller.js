55aeea31b89699b4b2a63bdf51df397634e71fea
Ammar Ahmed Khan <mrahmed_44@hotmail.com>
2023-08-13 13:01:09 +0800

    filter feature

--- controller/View7.controller.js
+++ controller/View7.controller.js
@@ -3,9 +3,10 @@ sap.ui.define([
     "sap/m/MenuItem",
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
+	'sap/ui/core/Fragment',
     "../utils/utilFn"
 
-], function(MessageToast,MenuItem, Controller, JSONModel, utilFn) {
+], function(MessageToast,MenuItem, Controller, JSONModel, Fragment,utilFn) {
 
     "use strict";
     const todaysDate = new Date();
@@ -26,10 +27,17 @@ sap.ui.define([
             descValueState: sap.ui.core.ValueState.None
         }]
     });
+	
+	const filterSwitch = new JSONModel({showPendingSwitch : false, showPendingItems : false,
+	showToPayItems: false});
+	let aFilters = [];
     return Controller.extend("sap.m.sample.sampleapp4.controller.View7", {
 
         onInit: function() {
             this.getView().setModel(oTable, 'table');
+            this.getView().setModel(filterSwitch, 'switch');
+			this._mDialogs = {};
+			
         },
         addExp: function() {
             oTable.oData.rows.push({
@@ -68,6 +76,10 @@ sap.ui.define([
 				this.performCalc();
 			}
 		},
+		resetFilters : function(oEvent){
+			this.filterItems("");
+			aFilters = [];
+		},
 		doCalc: function(attrToFilter,booleanVal,attrToUpdate){
 			graphCalcsTotalFigures.oData[attrToUpdate]=0;
 			switch(oTable.oData.rows.filter((object) => {
@@ -99,38 +111,61 @@ sap.ui.define([
 		this.getView().setModel(graphCalcsTotalFigures, 'graphCalcsTotalFigures');		
         },
 		
-		onMenuAction: function(oEvent) {
-				var oItem = oEvent.getParameter("item"),
-					sItemPath = "";
+		
+		// special thanks to 
+		// https://sapui5.hana.ondemand.com/#/entity/sap.m.ViewSettingsDialog/sample/sap.m.sample.ViewSettingsDialog/code
+		// View Setting Dialog opener
+		_openDialog : function () {
+			var oView = this.getView();
+			// creates requested dialog if not yet created
+			const sName = "Dialog";
+			if (!this._mDialogs[sName]) {
+				this._mDialogs[sName] = Fragment.load({
+					id: oView.getId(),
+					name: "sap.m.sample.sampleapp4.view.FilterViewDialog",
+					controller: this
+				}).then(function(oDialog){
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+			this._mDialogs[sName].then(function(oDialog){
+				// opens the requested dialog
+				oDialog.open();
+			});
+		},
 
-				while (oItem instanceof MenuItem) {
-					sItemPath = oItem.getText() + " > " + sItemPath;
-					oItem = oItem.getParent();
-				}
-				sItemPath = sItemPath.substr(0, sItemPath.lastIndexOf(" > "));
-
-				MessageToast.show("Action triggered on item: " + sItemPath);
-				switch(sItemPath){
-				case 'Show Paid/Recieved':
-					this.filterItems('paid_or_recieved');
-				case 'Show All':
-					this.filterItems('all');				
-				case 'Show Pending':
-					this.filterItems('pending');								
-				}
-				
-			},
+		// Opens View Settings Dialog
+		handleOpenDialog: function () {
+			this._openDialog();
+		},
+			
+		handleConfirm: function (oEvent) {
+			if (oEvent.getParameters().filterString) {
+				oEvent.getParameters().filterItems.map(object =>{
+					//console.log(object.mProperties.key);
+					this.filterItems(object.mProperties.key);
+				});
+			}
+		},
+	
 		filterItems : function(filterOption){
-        /*     if (filterOption == "all") {
-                jsonModel = new JSONModel(jsonData);
-                this.getView().setModel(jsonModel);
-                return;
-            }
-            const jsonDataFiltered = jsonData.filter(function(item) {
-                if (item.status == status) return true
-            });
-            jsonModel = new JSONModel(jsonDataFiltered);
-            this.getView().setModel(jsonModel); */
+		//https://answers.sap.com/questions/12625140/clear-filters-and-sorts-in-sapuitabletable.html
+		const oBinding = this.getView().byId('tbl').getBinding();
+		if(filterOption == "")
+		{
+		//oBinding.aFilters = null;
+		const oEmptyFilter = new sap.ui.model.Filter();
+		oBinding.filter(oEmptyFilter);
+		return;
+		}
+		else{
+		let filterQuery = filterOption.split(" ");
+		//const oFilter = new sap.ui.model.Filter("paid_or_received", "EQ", true);
+		const oFilter = new sap.ui.model.Filter(filterQuery[0], filterQuery[1], filterQuery[2] == "true");
+		aFilters.push(oFilter);
+		}
+		oBinding.filter(aFilters);
 		},
 
         onBack: function() {
